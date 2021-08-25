7ad29a540f93959d944d57a28f11139561c30eaf
Allie | アリー <me@sixaiy.com>
2021-08-25 23:10:47 +0800

    conf.js updates to match current setup

--- src/sample.conf.js
+++ src/sample.conf.js
@@ -24,31 +24,30 @@ module.exports = {
     api_token:              "",
 
     legacy_prefix:          "-", // Bot runs with Slash Commands Now.
+
+    bot_owners:             ["", ""], // Only place userIds in here.
+    bot_owner:              "", // Role for Owners
     bot_admin:              "",
     bot_mod:                "",
-    bot_owners:             [""], // Only place userIds in here.
+    bot_contributor:        "",
+
     guild_id:               "",
+    guild_invite:           "",
 
     channel_restrictions:   ["", ""],
     channel_rules:          "",
     channel_bot_spam:       "",
-
+    
     game_status:            "online",
-    game_name:              "",
+    game_name:              "/help or ,,help",
     game_type:              1,
 
     webhook_error_log:      "",
     webhook_guild_log:      "",
 
-    eris_shards:            "auto",
-    eris_all_users:         true,
-    eris_intents:           [ "guilds", "guildMembers", "guildMessages", "guildInteractions", "guildVoiceStates" ],
+    eris_options:           { maxShards: "auto", getAllUsers: true, intents: [ "guilds", "guildMembers", "guildMessages", "guildInteractions", "guildVoiceStates" ] },
 
     invite_perms:           "",
-    guild_invite:           "",
-
-    music_api:              "",
-    music_stream:           "",
 
     embed_color:             0x421250,
 
@@ -56,11 +55,34 @@ module.exports = {
     web_port:                4000,
 
     // Bot Listing Places
-    enable_post:             false,
+    enable_post:             true,
     sentcord_token:         "",
     carbonitex_token:       "",
     topgg_token:            "",
     discordbotsgg_token:    "",
     discordbotlist_token:   "",
-    discordlistspace_token: ""
+    discordlistspace_token: "",
+    dbotsco_token:          "",
+    discords_token:         "",
+    botsdiscordlabs_token:  "",
+    discordexlist_token:    "",
+
+    // GensokyoRadio Posting Locations
+    gr_api_key:             "",
+
+    gr_stream:              "https://stream.gensokyoradio.net/2",
+    gr_discord:             "https://discord.gg/5ecahrZ ",
+    gr_url:                 "https://gensokyoradio.net",
+
+    gr_api:                 "/api",
+    gr_api_discord:         "/discord",
+    gr_api_playing:         "/station/playing",
+    gr_register:            '/register',
+    gr_connect_user:        '/connect/user',
+    gr_update:              '/update',
+    gr_disconnect_user:     '/disconnect/user',
+    gr_connect_channel:     '/connect/channel',
+    gr_disconnect_channel:  '/disconnect/channel',
+    keyUnregister:          '',
+    gr_rating:              '/rating'
 };
