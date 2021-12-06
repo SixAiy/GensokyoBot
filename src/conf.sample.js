121da8d9d2f40eb125247b0d89f4d1a0afb4746f
Allie | アリー <me@sixaiy.com>
2021-12-06 23:39:25 +0800

    Update on conf.sample.js

--- src/conf.sample.js
+++ src/conf.sample.js
@@ -3,7 +3,7 @@
 # File: conf.js / sample.conf.js
 # Title: A Radio Music Bot
 # Author: SixAiy <me@sixaiy.com>
-# Version: 5.2
+# Version: 5.3
 # Description:
 #  A Discord bot for playing the Gensokyo Radio.
 #####################################################################
@@ -18,16 +18,19 @@
 "use strict"
 
 module.exports = {
-    version:    "5.2",
-
+    dev:        false, // if not runing main GensokyoBot
+    version:    "5.3_58",
+    
     bot:        "",
     api:        "",
     url:        "https://api.o7.fyi",
     stream:     "https://stream.gensokyoradio.net/2/",
     site:       "https://gensokyobot.com",
+    logs:       "899315347679744000",
     
     state:      "online", 
     option:     { name: "/help", type: 0 },
-    eris:       { maxShards: "auto", intents: [ "guilds", "guildMembers", "guildInteractions", "guildVoiceStates" ] },
+    eris:       { autoreconnect: true, maxShards: "auto", intents: [ "guilds", "guildMessages", "guildMembers", "guildInteractions", "guildVoiceStates" ] },
     color:      0x421250,
+    port:       4022
 };
